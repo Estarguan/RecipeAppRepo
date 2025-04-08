aabea715fccc773f4f13e49494f1db40e31899b2
Estarguan <estarguan1111@gmail.com>
2025-04-07 20:01:22 -0400

    First commit

---
+++ RecipeApp/RecipeApp/Views/MainView.swift
@@ -0,0 +1,37 @@
+//
+//  MainView.swift
+//  RecipeApp
+//
+//  Created by Estar Guan on 2025-04-05.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    @State var isAuthenticated = false
+    var body: some View {
+     
+        Group {
+          if isAuthenticated {
+              Text("Hello")
+            //ProfileView()
+          } else {
+              MenuView()
+          }
+        }
+        
+        .task {
+          for await state in supabase.auth.authStateChanges {
+            if [.initialSession, .signedIn, .signedOut].contains(state.event) {
+              isAuthenticated = state.session != nil
+            }
+          }
+        }
+      
+        
+    }
+}
+
+#Preview {
+    MainView()
+}
